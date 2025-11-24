fd2cd7e65274860c65519954b7ed328e8e19e716
Yein Lee <zxth47e@gmail.com>
2025-11-24 13:02:07 +0900

    chore: shadcn 컴포넌트 설치

---
+++ packages/after/src/components/index.ts
@@ -0,0 +1,6 @@
+export * from '@/components/ui/button';
+export * from '@/components/ui/card';
+export * from '@/components/ui/form';
+export * from '@/components/ui/input';
+export * from '@/components/ui/select';
+export * from '@/components/ui/table';
